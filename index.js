aed70978eb3b408d8a72a3b9ae8cc4e166073bc6
Jayklef <jerryarhawho@gmail.com>
2024-02-16 21:25:17 +0000

    Defining functions

---
+++ index.js
@@ -0,0 +1,25 @@
+const weatherForm = document.querySelector("#weatherForm");
+const cityDiplay = document.querySelector(".cityDisplay");
+const card = document.querySelector(".card");
+const apikey = "d9702ec388a7c1a95af4b3d86766ff5d";
+
+
+weatherForm.addEventListener("submit", e =>{
+    e.preventDefault
+});
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function displayError(message){
+    
+}
